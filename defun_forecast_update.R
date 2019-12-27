a61d7842e974e62db59c3e021c61d8dad43f30ea
Scott Jenkins <srj@posteo.org>
2019-12-27 11:21:52 -0500

    Initial script for calling functions for updating forecasts on a recurring basis.

---
+++ defun_forecast_update.R
@@ -0,0 +1,10 @@
+# defun_forecast_update.R 
+#################################################
+# Description: Calls functions requiring update.
+
+# Sources
+source("defun_yield.R")
+source("defun_nasdaq.R")
+source("defun_yield.R")
+
+defun_yield()
