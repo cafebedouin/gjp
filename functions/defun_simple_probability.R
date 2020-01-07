c3903f4b165c52251459e4e060bc80a942122e0c
Scott Jenkins <srj@posteo.org>
2020-01-07 14:53:19 -0600

    Added frequency functionality for probabilities.

--- functions/defun_simple_probability.R
+++ functions/defun_simple_probability.R
@@ -1,7 +1,7 @@
-# defun_simple_probability.R 
+# defun_simple_qprob.R 
 #################################################
 # Description: This function takes a dataframe 
-# with two columns (date, value). The first as.Date,
+# with two columns (time, value). The first as.Date,
 # in ISO-8601 format, in reverse chronological order, 
 # and the second as a vector of numerical values.
 # Example: 2019-08-06,1.73
@@ -19,14 +19,17 @@
 # 0.084: Bin 5 - 3+
 # Number of observations: 1877
 
-defun_simple_probability <- function(df,
-                                     prob_results_title="",
-                                     closing_date,
-                                     trading_days,
-                                     bin1,
-                                     bin2,
-                                     bin3,
-                                     bin4) {
+defun_simple_qprob <- function(df,
+                               prob_results_title,
+                               closing_date,
+  # freq options: daily, weekly, monthly, quarterly or yearly
+                               freq="daily",
+  # trading_days is only relevant for daily data
+                               trading_days=7,
+                               bin1,
+                               bin2,
+                               bin3,
+                               bin4) {
   
   #################################################
   # Calculate time
@@ -34,10 +37,20 @@ defun_simple_probability <- function(df,
   closing_date <- as.Date(closing_date)
   remaining_weeks <- as.numeric(difftime(closing_date, todays_date, units = "weeks"))
   remaining_weeks <- round(remaining_weeks, digits=0)
-  non_trading_days <- (7 - trading_days) * remaining_weeks
-  day_difference <- as.numeric(difftime(closing_date, todays_date))
-  remaining_days <- day_difference - non_trading_days 
+  remaining_time <- remaining_weeks
+  
+  # Convert weeks into period of interest
+  if (freq == "daily") {
+    non_trading_days <- (7 - trading_days) * remaining_weeks
+    day_difference <- as.numeric(difftime(closing_date, todays_date))
+    remaining_time <- day_difference - non_trading_days 
+  }  
 
+#  if (freq == "weekly") { remaining_time <- remaining_weeks }
+  if (freq == "monthly") { remaining_time <- remaining_weeks / 4 }  
+  if (freq == "quarterly") { remaining_time <- remaining_weeks / 13 }
+  if (freq == "yearly") { remaining_time <- remaining_weeks / 52 }
+    
   #################################################
   # Check formating
   
@@ -58,8 +71,8 @@ defun_simple_probability <- function(df,
   # Setting most recent value, assuming descending data
   current_value <- as.numeric(df[1,2])
 
-  # Get the length of df$value and shorten it by remaining_days
-  prob_rows = length(df$value) - remaining_days
+  # Get the length of df$value and shorten it by remaining_time
+  prob_rows = length(df$value) - remaining_time
 
   # Create a dataframe
   prob_calc <- NULL
@@ -67,7 +80,7 @@ defun_simple_probability <- function(df,
   # Iterate through value and subtract the difference 
   # from the row remaining days away.
   for (i in 1:prob_rows) {
-    prob_calc[i] <- df$value[i] - df$value[i+remaining_days]
+    prob_calc[i] <- df$value[i] - df$value[i+remaining_time]
   }
 
   # Adjusted against current values to match prob_calc
@@ -91,5 +104,5 @@ defun_simple_probability <- function(df,
                   prob3, ": Bin 3 - ", bin2, "+ to <", bin3, "\n", 
                   prob4, ": Bin 4 - ", bin3, " to <=", bin4, "\n", 
                   prob5, ": Bin 5 - ", bin4, "+", "\n",
-                  "Number of observations: ", prob_rows)))
+                  "Number of observations: ", prob_rows, "\n")))
 }
