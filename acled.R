a1ad0b98133521e595fc2575d537f211aabd403d
cafebedouin <srj@posteo.org>
2020-07-10 01:54:22 +0000

    Working acled.R for monthly event probability based on history.

--- acled.R
+++ acled.R
@@ -1,18 +1,27 @@
-# acled_riots.R
-# (c) Ezra Karger, May 14, 2018
-# Modified by Scott Jenkins, <srj@posteo.org>
-# Last revised: December 4, 2018 
+# acled.R
+# (c) Scott Jenkins, <srj@posteo.org>, July 9, 2020
+# Adapted from: 
+# https://github.com/jeremy-allen/getACLED/blob/master/acled_api_pull.R
+# Last revised: July 9, 2020 
 
 #################################################
 # Description: Pulls data from ACLED by country 
-# for battle, creates csv and makes prediction
 
-#Clear memory
+# Clear memory
 rm(list=ls())
 gc()
 
-acled_riots <- function(country="Gabon") {
-
+acled <- function(country="Ethiopia",
+                  event="Riots",
+                  year=NULL) {
+  
+  # Events can include: NULL (includes all), "Battles", "Violence against civilians", 
+  # "Explosions/Remote violence", "Riots, "Protests", "Strategic developments" 
+  
+  
+  ###################################################
+  # Preliminaries
+  
   #Preventing scientific notation in graphs
   options(scipen=999)
 
@@ -26,117 +35,100 @@ acled_riots <- function(country="Gabon") {
   library(ggplot2)
   library(plyr)
   library(RCurl)
+  
+  #Replace spaces in country name with space code:
+  country <- str_replace_all(country, " ", "%20")
 
   ###############################################
   # ACLED DATA FUNCTION
-
-  #This is adapted from https://github.com/jeremy-allen/getACLED/blob/master/acled_api_pull.R
-  getACLED <- function(country = "Ethiopia", year = NULL) {
-    
-    #This is the base URL for the api:
-    myurl <- "https://api.acleddata.com/acled/read?&terms=accept&"
   
-    #Replace spaces in country name with space code:
-    country2 <- str_replace_all(country, " ", "%20")
+  aurl <- "https://api.acleddata.com/acled/read?&terms=accept&limit=10000"
   
-    #Define API call, which depends on whether you are searching for
-    # a country or a year:
-    if(is.null(country2) & is.null(year)) {
-      myurl <- "https://api.acleddata.com/acled/read?&terms=accept&limit=10000"
-    } else if (!is.null(country2) & is.null(year)) {
-      myurl <- paste0(myurl, "&country=", country2, "&country_where=%3D", "&limit=0")
-    } else if (is.null(country2) & !is.null(year)) {
-      myurl <- paste0(myurl, "&year=", year, "&limit=0")
+    #Define API url:
+    if(is.null(country) & is.null(year)) {
+      aurl <- "https://api.acleddata.com/acled/read?&terms=accept&limit=10000"
+    } else if (!is.null(country) & is.null(year)) {
+      aurl <- paste0(aurl, "&country=", country, "&country_where=%3D", "&limit=0")
+    } else if (is.null(country) & !is.null(year)) {
+      aurl <- paste0(aurl, "&year=", year, "&limit=0")
     } else {
-      myurl <- paste0(myurl, "&country=", country2, "&country_where=%3D", "&year=", year, "&limit=0")
+      aurl <- paste0(aurl, "&country=", country, "&country_where=%3D", "&year=", year, "&limit=0")
     }
 
-  
     # Download the data! The API returns a list:
-    acledEvents <- fromJSON(myurl)
+    df <- fromJSON(aurl) 
     
-    View(acledEvents)
-  }
-}
-
-  
     #Select just the data from the returned list:
-    acledEvents <- as.data.table(acledEvents[[4]])
+    df <- as.data.table(df[[5]])
   
-    #Drop 'data.' from all column names:
-    names(acledEvents) <- str_replace_all(names(acledEvents), 'data.', '')
-
     #Convert event_date column to date class:
-    acledEvents[, event_date := ymd(event_date)]
+    df[, event_date := ymd(event_date)]
   
     #Convert latitude column to double(numeric) class:
-    acledEvents[, latitude := as.double(latitude)]
+    df[, latitude := as.double(latitude)]
   
     #Convert longitude column to double(numeric) class:
-    acledEvents[, longitude := as.double(longitude)]
+    df[, longitude := as.double(longitude)]
   
     #Convert fatalities column to double(numeric) class:
-    acledEvents[, fatalities := as.numeric(fatalities)]
+    df[, fatalities := as.numeric(fatalities)]
   
     #Define the month and the year of each event:
-    acledEvents$month = as.numeric(substr(acledEvents$event_date,6,7))
-    acledEvents$year = as.numeric(acledEvents$year)
+    df$month = as.numeric(substr(df$event_date,6,7))
+    df$year = as.numeric(df$year)
   
-    #Restrict data to observations post 2009 (I don't trust the pre-2010 data)
-    acledEvents = acledEvents[which(acledEvents$year>=2011),]
-  
-    return(acledEvents)
-  }
+    # Limit to since 2013
+    df <- df[which(df$year>=2013),]
+    
+    View(df)
+}
+    
+    df <- df[which(df$event_type==paste0(event)),]
+    
+    ###############################################
+    # EVENT ANALYSIS:
+    
+    # Identify events, summarize at the month level, and rename variables:
+    events <- ddply(df, .(df$year, df$month), nrow)
+    names(events) = c("year","month","frequency")
 
-  ###############################################
-  #RIOT/PROTEST ANALYSIS:
-
-  #Download all events for country
-  data_riots <- getACLED()
-
-  #Identify riots, summarize at the month level, and rename variables:
-  data_riots_hit = data_riots[which(data_riots$event_type == "Riots/Protests"),]
-  counts_riots <- ddply(data_riots_hit, .(data_riots_hit$year, data_riots_hit$month), nrow)
-  names(counts_riots) = c("year","month","frequency")
-
-  riot_date <- Sys.Date()
-  riot_year <- as.numeric(gsub("-..-..", "", riot_date))
-  riot_month <- gsub("....-", "", riot_date)
-  riot_month <- as.numeric(gsub("-..", "", riot_month))
+    todays_date <- Sys.Date()
+    events_year <- as.numeric(gsub("-..-..", "", todays_date))
+    events_month <- gsub("....-", "", todays_date)
+    events_month <- as.numeric(gsub("-..", "", events_month))
   
   #Add observations for months with no events, and fill in zeroes:
-  for (yearval in 2010:riot_year){
+  for (yearval in 2013:events_year){
     for (monthval in 1:12){
-    
-      if (nrow(counts_riots[which(counts_riots$year==yearval & counts_riots$month==monthval),])==0 &
-          (yearval<riot_year | monthval<=riot_month)){
+      if (nrow(events[which(events$year==yearval & events$month==monthval),])==0 &
+          (yearval<events_year | monthval<=events_month)){
       
-            counts_riots = rbind(counts_riots,c(yearval,monthval,0))
+            events = rbind(events,c(yearval,monthval,0))
       }
     }
   }
 
   #Check for seasonality with a linear regression:
-  summary(lm(frequency~as.factor(month)+as.factor(year),data=counts_riots))
+  summary(lm(frequency~as.factor(month)+as.factor(year),data=events))
 
   #How many months had a riot/protest?
-  riot_odds <- as.data.frame(table(counts_riots$frequency>=1))
-  riot_yes <- riot_odds[2,2]
-  riot_no <- riot_odds[1,2]
-  riot_total_months <- riot_yes + riot_no
-  riot_probability <- riot_yes / riot_total_months
-  counts_riots = counts_riots[with(counts_riots, order(year, month)), ]
+  event_odds <- as.data.frame(table(events$frequency>=1))
+  event_yes <- event_odds[2,2]
+  event_no <- event_odds[1,2]
+  event_total_months <- event_yes + event_no
+  event_probability <- event_yes / event_total_months
+  events = events[with(events, order(year, month)), ]
   
   # Creating a cvs file of riot data
-  write.csv(counts_riots, file=paste0("./output/riots-in-", country, "-table-",
-                                   riot_date, ".csv")) 
+  write.csv(events, file=paste0("./", event, "-in-", country, "-table-",
+                                   todays_date, ".csv")) 
   
   return(cat(paste0("Historical Probability \n",
-                    "Consult riots-in-", country, 
+                    "Consult events-in-", country, 
                     " csv file to check base rate against recent trends.\n\n",
-                    "Months riot occured in ", country, " / ",
-                    "possible months riots could occur = ",
-                    "historical possiblity of riot. \n",
-                    riot_yes, " / ", riot_total_months, " = ", 
-                    riot_probability, " chance of a riot. \n")))
+                    "Months event occured in ", country, " / ",
+                    "possible months events could occur = ",
+                    "historical possiblity of ", event,  ". \n",
+                    event_yes, " / ", event_total_months, " = ", 
+                    event_probability, " chance of a ", event, ". \n")))
 }
