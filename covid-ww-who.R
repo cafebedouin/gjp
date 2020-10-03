b98382082a8e8d1ba0ba8c01131c46e85d51ecba
Scott Jenkins <srj@posteo.org>
2020-10-02 20:09:44 -0500

    "Script to review worldwide daily new deaths from COVID-19 from WHO."

--- covid-ww-who.R
+++ covid-ww-who.R
@@ -1,8 +1,5 @@
 # covid-ww-who.R
-# (c) Scott Jenkins, <srj@posteo.org>
-# Original: April 15, 2020
-# Last revised: April 15, 2018 
-
+# cafebedouin@gmail.com, 2020
 #################################################
 # Description:
 # Script parses cvs file and provides a graph 
@@ -15,8 +12,8 @@
 rm(list=ls())
 gc()
 
-covid <- function(country = "United States",
-                  end_date = "2020-04-25",
+covid <- function(country = "",
+                  end_date = "2021-04-01",
                   end_value = "600") {
   
   #################################################
@@ -49,48 +46,73 @@ covid <- function(country = "United States",
   # Download data for testing script 
   df <- read.csv(paste0("~/Downloads/WHO-COVID-19-global-data.csv"), 
                  na.strings = "", fileEncoding = "UTF-8-BOM",
-                 skip=0, header=FALSE)
+                 skip=0, header=TRUE)
 
   # Live import, switch to testing line when modifying script
-  # df <- read.csv("https://opendata.ecdc.europa.eu/covid19/casedistribution/csv", 
-  #                 na.strings = "", fileEncoding = "UTF-8-BOM")
-
-  # Sort by date column
-  df <- df[order(as.Date(df$V1)),]
+  #df <- read.csv("https://covid19.who.int/WHO-COVID-19-global-data.csv", 
+  #                 na.strings = "", header=TRUE, fileEncoding = "UTF-8-BOM")
   
   # Drop all the columns but the ones of interest
-  df <- df[ -c(2,4) ] 
+  df <- df[ -c(2,4:6,8) ] 
   
   # Add column names
   colnames(df)[1] <- "date"
   colnames(df)[2] <- "country"
-  colnames(df)[3] <- "new_deaths"
-  colnames(df)[4] <- "deaths"
-  colnames(df)[5] <- "new_cases"
-  colnames(df)[6] <- "cases"
+  colnames(df)[3] <- "deaths"
   
-  # Set datatypes
-  df$date <- as.Date(df$date) 
+  # Set datatypes  
+  df$date <- as.Date(df$date, origin="1970-01-01")
   df$country <- as.character(df$country)
-  df$new_deaths <- as.numeric(df$new_deaths) 
-  df$deaths <- as.numeric(df$deaths)   
-  df$new_cases <- as.numeric(df$new_cases) 
-  df$cases <- as.numeric(df$cases) 
+  df$deaths <- as.numeric(df$deaths) 
+  
+  # Sort by date column
+  df <- df[order(as.Date(df$date)),]
   
   # Limit by data of interest
-  df <- filter(df, country == "United States of America")
-#  df <- filter(df, date > "2020-03-15")
+  # df <- filter(df, country == "United States of America")
+  # df <- filter(df, date > "2020-03-15")
   
-  # Drop all the columns but the ones of interest
-  df <- df[ -c(2,3,5,6) ] 
+  # Create data frame
+  ww <- NULL
   
-  View(df)
+  # Reshape data, date rows and country columns 
+  ww <- reshape(df, direction="wide", idvar="date", timevar="country")
+  
+  # Fix the names on the columns
+  names(ww) <- gsub("deaths.", "", names(ww))
+  
+  ww[is.na(ww)] <- 0
+  
+  # Drop dates from count
+  count  <- ww[ -c(1) ]
+  
+  # Sum by date
+  ww$ww <- rowSums(count)
+  
+  # ww only
+  # ww <- ww[ c(1,67) ]
+  
+  # Check if number of columns has changed
+  # View(ww)
+
+  # ww only
+  ww <- ww[ -c(2:236) ]
+  
+  # Uncomment for cumulative numbers, comment out for daily deaths
+  # ww[, 2] <- cumsum(ww[, 2])
+  
+  # Add in end date and value 
+  
+  # df_add <- read.csv(paste0("~/Downloads/ww-end.csv"), 
+  #                   skip=0, header=TRUE)
+  
+  # ww <- rbind(ww, df_add)
   
   plot <- ggplot() + 
     geom_line(data = df, aes(x = date, y = deaths)) +
 #    geom_vline(xintercept=as.numeric(as.Date(todays_date-1)), linetype=4, colour="black") +
     bbc_style() +
-    labs(title=paste0("Deaths in ", country, ", ", todays_date),
+    labs(title=paste0("New Deaths Worldwide, ", todays_date),
          subtitle = "None") +
     scale_x_date()
   
@@ -104,5 +126,9 @@ covid <- function(country = "United States",
                 height_pixels =1000,
                 logo_image_path = paste0("./branding/logo.png"))
   
+  ###############################################
+  # Printing
+  # Creating a cvs file of changed data
+  write.csv(ww, file=paste0("./output/covid-ww-who-new-deaths-", todays_date, ".csv")) 
 }
-
+  
